d25421a168aa0126a5e0930b082fab5b442c644d
ncoder <bhargavayash2015@gmail.com>
2024-04-17 17:36:47 +0530

    bucket-url

--- backend/services/getObjectPdf.js
+++ backend/services/getObjectPdf.js
@@ -1,14 +1,13 @@
 const { GetObjectCommand } = require("@aws-sdk/client-s3");
 const { s3Client } = require("../client/S3_client");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-
+require("dotenv").config();
 async function getObject(filename) {
     const command = new GetObjectCommand({
-        Bucket: `pdf-extract-ncoder`,
+        Bucket: `${process.env.BUCKET_URL}`,
         Key: filename,
     })
     const url = await getSignedUrl(s3Client, command);
-    // console.log(url);
     return url;
 } 
 
--- backend/services/setObjectPdf.js
+++ backend/services/setObjectPdf.js
@@ -1,10 +1,10 @@
 const { PutObjectCommand } = require("@aws-sdk/client-s3");
 const { s3Client } = require("../client/S3_client");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-
+require("dotenv").config();
 async function putObject(filename) {
     const command = new PutObjectCommand({
-        Bucket: `pdf-extract-ncoder`,
+        Bucket: `${process.env.BUCKET_URL}`,
         Key: `${filename.split(".")[0]}.pdf`,
         ContentType: `pdf`
     })
